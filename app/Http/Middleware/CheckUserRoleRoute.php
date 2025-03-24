c5fcadd8827c047f04ec7c6c29520b2e597b9eb6
root <root@DESKTOP-5N0DFPB>
2025-03-24 21:43:06 +0500

    Redirect User to show page if edit permission is not given and show permission is given

--- app/Http/Middleware/CheckUserRoleRoute.php
+++ app/Http/Middleware/CheckUserRoleRoute.php
@@ -14,6 +14,7 @@ class CheckUserRoleRoute
     public function handle(Request $request, Closure $next): Response
     {
         $requested_route = $request->route()->getName();
+
         $always_allow_routes = [null, 'login', 'login.form', 'login.submit', 'logout', 'home'];
         
         if(in_array($requested_route, $always_allow_routes)) {
@@ -22,14 +23,16 @@ class CheckUserRoleRoute
         
         $allowed_routes = session('user_routes');
 
-        // echo "<pre><strong>" . __FILE__ . " Line: [". __LINE__ ."]</strong> @ " .date("Y-m-d H:i:s"). "<br>"; print_r( $requested_route ); echo "</pre><br>";
-        // echo "<pre><strong>" . __FILE__ . " Line: [". __LINE__ ."]</strong> @ " .date("Y-m-d H:i:s"). "<br>"; print_r( $allowed_routes ); echo "</pre><br>"; exit;
+        // If edit not allowed, send to show if allowed
+        if (isset($allowed_routes) && $requested_route === 'service_requests.edit' && !isset($allowed_routes[$requested_route]) && isset($allowed_routes['service_requests.show'])) {
+            $id = $request->route('service_request');
+            return redirect()->route('service_requests.show', ['service_request' => $id])->with('info', 'You do not have permission to edit this Service Request. Viewing instead.');
+
+        }
         
         if(isset($allowed_routes) && isset($allowed_routes[$requested_route])) {
             return $next($request);
-        }
-        else
-        {
+        } else {
             return redirect()->back()->with('error', 'You do not have Permission to perform this action!');
         }
 
